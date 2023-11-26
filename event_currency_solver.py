b130cf939af6d505dada97ef7cc854f266c29151
TanapTheTimid <tanapoom.se@gmail.com>
2023-11-26 16:26:24 +0000

    add ability to set upper bound per stage

--- event_currency_solver.py
+++ event_currency_solver.py
@@ -6,27 +6,29 @@ AP_USAGE_LIST = [ 10, 10, 10, 10,   15, 15, 15, 15,   20, 20, 20, 20]
 
 # List of resources given by each stage (with bonus)
 # The rows are the resources and the columns are the stages
-RESOURCE_LIST = [[21,  0,  0, 26,   32,  0,  0, 42,   45, 26, 26, 64],
-                 [ 2, 13,  4,  0,    4, 20,  8,  0,    8, 28,  0,  0],
-                 [ 3,  6, 20,  0,    6, 12, 30,  0,   12,  0, 42,  0],]
+RESOURCE_LIST = [[23,  0,  0, 35,   37,  0,  0, 52,   54,  8,  8, 69],
+                 [ 5, 21,  6,  0,    6, 34,  6,  0,    6, 47,  0,  0],
+                 [ 6,  8, 26,  0,    8,  8, 42,  0,    8,  0, 58,  0],]
 
 # Currency target
-REQUIREMENT_LIST = [8588, 1920, 10225]
+REQUIREMENT_LIST = [20000, 15675, 14650]
 
 # The amount of AP used to clear initially
-INITIAL_CLEAR_AP = 285
+INITIAL_CLEAR_AP = 305
 
 # The amount of currency received in the initial clear
-INITIAL_CLEAR = [1335, 867, 867]
+INITIAL_CLEAR = [2248, 1588, 1588]
 
+# Upper Bound - set to 0 for each stage you want to avoid
+i = 1000000
+var_upper_bound = [0,0,0,0, 0,0,0,0, i,i,i,i]
 
 
 # Solver
 model = cp_model.CpModel()
-var_upper_bound = 1000000
 qs = []
 for i in range(1,len(AP_USAGE_LIST)+1):
-    qs += [model.NewIntVar(0, var_upper_bound, "q" + str(i))]
+    qs += [model.NewIntVar(0, var_upper_bound[i-1], "q" + str(i))]
 
 for j, constraint in enumerate(RESOURCE_LIST):
     f_i = constraint[0] * qs[0]
